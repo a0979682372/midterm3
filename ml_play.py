97e03cec781aa78f68256b2eeedf18b65b7c0930
a0979682372 <38906793+a0979682372@users.noreply.github.com>
2020-06-18 11:30:57 +0800

    Add files via upload

--- ml_play.py
+++ ml_play.py
@@ -95,7 +95,17 @@ class MLPlay:
             if len(grid) == 0:
                 return ["SPEED"]
             else:
-                
+                if (2 not in grid) :
+
+                    if (13 in grid):
+                        if (12 not in grid):
+                            return ["SPEED", "MOVE_LEFT"]
+                        elif (14 not in grid):
+                            return ["SPEED", "MOVE_RIGHT"]
+                        elif (self.h2>self.h3) and (self.h2>self.h4):
+                            return ["SPEED", "MOVE_LEFT"]
+                        elif (self.h4>self.h3):
+                            return ["SPEED", "MOVE_RIGHT"]
 
                 if (2 not in grid): # Check forward 
                     # Back to lane center
